cb3ca33c7dccff27dcd7dd10345abf8083eed56d
EddyKaggia <kagzcodes@gmail.com>
2024-04-24 13:13:48 -0700

    feat: methods in Arrays and ArrayLists

--- src/main/java/com/eddykaggia/app/ArrayAndArrayList.java
+++ src/main/java/com/eddykaggia/app/ArrayAndArrayList.java
@@ -6,11 +6,25 @@ import java.util.Arrays;
 public class ArrayAndArrayList {
     public static void main(String[] args) {
         // Initializing a new Array
-        // Arrays have a FIXED size
+        // Arrays have a FIXED size -> Can't GROW or SHRINK
         String[] friendsArray = new String[4];
-        String[] friendsArray2 = {"Issa", "Molly", "Kelly", "Tiffany"};
+        String[] friendsArray2 = {"Issa", "Molly", "Kelli", "Tiffany"};
         System.out.println(Arrays.toString(friendsArray2));
-        //
+
+        // Accessing elems in Array
+        System.out.println(friendsArray2[3]); // Tiffany
+
+        // Checking the size/length of an Array
+        System.out.println(friendsArray2.length); // 4
+
+        // Set an element
+        friendsArray2[0] = "Lawrence";
+        System.out.println(Arrays.toString(friendsArray2));
+
+        // Have to swap Lawrence out coz ...
+        friendsArray2[0] = "Issa";
+        System.out.println(Arrays.toString(friendsArray2));
+
         System.out.println();
 
         // Initializing a new ArrayList
@@ -19,6 +33,27 @@ public class ArrayAndArrayList {
         ArrayList<String> friendsArrayList2 =
                 new ArrayList<>(Arrays.asList("Earn", "Van", "Paper Boi", "Darius"));
         System.out.println(friendsArrayList2);
-        System.out.println();
+
+        // Accessing elems in ArrayList
+        System.out.println(friendsArrayList2.get(3)); // Darius
+
+        // Checking the size/length of an ArrayList
+        System.out.println(friendsArrayList2.size()); // 4
+
+        // Add an element
+        friendsArrayList2.add("Earnest Marks");
+        System.out.println(friendsArrayList2);
+
+        // Set an element
+        friendsArrayList2.set(0, "Tracy");
+        System.out.println(friendsArrayList2);
+
+        // Remove an element
+        friendsArrayList2.remove("Tracy");
+        System.out.println(friendsArrayList2);
+
+        // Have to add Earn back in coz ...
+        friendsArrayList2.add("Earn");
+        System.out.println(friendsArrayList2);
     }
 }
